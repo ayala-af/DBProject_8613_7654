def9a4abdeb0f8538146f49251b0d3393d98465a
ayala-af <ayalaaftergut@gmail.com>
2024-05-24 00:18:50 +0300

    filling some of the tabels

---
+++ sql/selectAll.sql
@@ -0,0 +1,29 @@
+select * from CITY;
+select count(*) from CITY;
+
+select * from TEACH;
+select count(*) from TEACH;
+
+select * from PARTICIPATE;
+select count(*) from PARTICIPATE;
+
+select * from  CLIENTS;
+select count(*) from  CLIENTS;
+
+select * from  SCHEDULER;
+select count(*) from  SCHEDULER;
+
+select * from  GROUPS_;
+select count(*) from  GROUPS_;
+
+select * from  AGE_GROUP;
+select count(*) from  AGE_GROUP;
+
+select * from  ROOMS;
+select count(*) from  ROOMS;
+
+select * from  TEACHERS;
+select count(*) from  TEACHERS;
+
+select * from  WORKSHOPS;
+select count(*) from  WORKSHOPS;
